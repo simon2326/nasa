03bf92498c51198b042d603f07f83395b6bf8f4d
Simon <simon.correa@upb.edu.co>
2023-01-31 15:47:39 -0500

    cambios en prox.html y prox.js

--- js/prox.js
+++ js/prox.js
@@ -1,7 +1,11 @@
+/*Series */
 (function(){
     const sliders = [...document.querySelectorAll('.series-body')];
+    const slidersp = [...document.querySelectorAll('.pel-body')];
     const buttonNext = document.querySelector('#siguiente');
     const buttonBefore = document.querySelector('#anterior');
+    const buttonNextp = document.querySelector('#siguientep');
+    const buttonBeforep = document.querySelector('#anteriorp');
     let value;
 
     buttonBefore.addEventListener('click', ()=>{
@@ -10,6 +14,14 @@
 
     buttonNext.addEventListener('click', ()=>{
         changePosition(1);
+    });
+
+    buttonBeforep.addEventListener('click', ()=>{
+        changePositionp(-1);
+    });
+
+    buttonNextp.addEventListener('click', ()=>{
+        changePositionp(1);
     });
 
     const changePosition = (add)=>{
@@ -24,5 +36,19 @@
         
         sliders[value-1].classList.add('series-body-show');
     }
+
+    const changePositionp = (add)=>{
+        const currentSliderp = document.querySelector('.pel-body-show').dataset.id;
+        value = Number(currentSliderp);
+        value += add;
+
+        slidersp[Number(currentSliderp)-1].classList.remove('pel-body-show');
+        if(value === slidersp.length+1 || value===0){
+            value = value === 0 ? slidersp.length : 1;
+        }
+        
+        slidersp[value-1].classList.add('pel-body-show');
+    }
 })();
+
 
